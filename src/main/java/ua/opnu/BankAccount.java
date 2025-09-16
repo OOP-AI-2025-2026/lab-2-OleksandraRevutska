8bb48ae49a60060b3c849ebe36ce83a7bbf47584
9103053@stud.op.edu.ua <9103053@stud.op.edu.ua>
2025-09-16 07:03:37 +0300

    lab2 completed

--- src/main/java/ua/opnu/BankAccount.java
+++ src/main/java/ua/opnu/BankAccount.java
@@ -5,23 +5,52 @@ public class BankAccount {
     double balance;
     double transactionFee;
 
+    // Порожній конструктор
+    public BankAccount() {
+        this.name = "";
+        this.balance = 0;
+        this.transactionFee = 0;
+    }
+
+    // Конструктор з ім'ям і балансом
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+        this.transactionFee = 0;
+    }
+
+    // Додає гроші на рахунок, тільки якщо amount > 0
     void deposit(double amount) {
-        // TODO: modify method body
-        balance = balance + amount;
+        if (amount > 0) balance += amount;
     }
 
-    double getBalance() {
-        return this.balance;
-    }
-
+    // Знімає гроші з рахунку з урахуванням комісії
     boolean withdraw(double amount) {
-        // TODO: modify method body
-        balance = balance - amount;
-        return true;
+        if (amount <= 0) return false;
+        if (balance >= amount + transactionFee) {
+            balance -= amount + transactionFee;
+            return true;
+        }
+        return false;
     }
 
+    // Переказ грошей іншому рахунку
     boolean transfer(BankAccount receiver, double amount) {
-        // TODO: modify method body
+        if (amount <= 0) return false;
+        if (withdraw(amount)) {  // знімає гроші з комісією
+            receiver.deposit(amount);  // додає гроші на інший рахунок
+            return true;
+        }
         return false;
+    }
+
+    // Повертає баланс
+     double getBalance() {
+        return balance;
+    }
+
+    // Встановлює комісію
+    void setTransactionFee(double fee) {
+        if (fee >= 0) transactionFee = fee;
     }
 }
--- src/main/java/ua/opnu/TimeSpan.java
+++ src/main/java/ua/opnu/TimeSpan.java
@@ -1,46 +1,87 @@
 package ua.opnu;
 
-
 public class TimeSpan {
+    private int hours;
+    private int minutes;
 
-    // TODO: add class fields
-
+    // Конструктор
     TimeSpan(int hours, int minutes) {
-        // TODO: write constructor body
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            // якщо аргументи неправильні, ставимо 0
+            this.hours = 0;
+            this.minutes = 0;
+        } else {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
     }
 
+    // Повертає тільки години
     int getHours() {
-        return 0;
+        return hours;
     }
 
+    // Повертає тільки хвилини
     int getMinutes() {
-        // TODO: write method body
-        return 0;
+        return minutes;
     }
 
+    // Додає години і хвилини
     void add(int hours, int minutes) {
-        // TODO: write method body
+        if (hours < 0 || minutes < 0 || minutes > 59) {
+            // якщо аргументи неправильні, нічого не робимо
+            return;
+        }
+
+        this.hours += hours;
+        this.minutes += minutes;
+
+        // якщо хвилини >= 60, переводимо в години
+        if (this.minutes >= 60) {
+            this.hours += this.minutes / 60;
+            this.minutes = this.minutes % 60;
+        }
     }
 
+    // Додає інший інтервал
     void addTimeSpan(TimeSpan timespan) {
-        // TODO: write method body
+        add(timespan.getHours(), timespan.getMinutes());
     }
 
+    // Загальна кількість годин у дробовому вигляді
     double getTotalHours() {
-        // TODO: write method body
-        return 0;
+        return hours + minutes / 60.0;
     }
 
+    // Загальна кількість хвилин
     int getTotalMinutes() {
-        // TODO: write method body
-        return 0;
+        return hours * 60 + minutes;
     }
 
+    // Віднімає інший інтервал
     void subtract(TimeSpan span) {
-        // TODO: write method body
+        int totalMinutesThis = getTotalMinutes();
+        int totalMinutesOther = span.getTotalMinutes();
+
+        if (totalMinutesOther > totalMinutesThis) {
+            // якщо віднімати більше, нічого не робимо
+            return;
+        }
+
+        int diff = totalMinutesThis - totalMinutesOther;
+        this.hours = diff / 60;
+        this.minutes = diff % 60;
     }
 
+    // Масштабує інтервал у factor разів
     void scale(int factor) {
-        // TODO: write method body
+        if (factor <= 0) {
+            // якщо factor <= 0, нічого не робимо
+            return;
+        }
+
+        int total = getTotalMinutes() * factor;
+        this.hours = total / 60;
+        this.minutes = total % 60;
     }
 }
